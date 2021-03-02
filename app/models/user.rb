2722d13eb01b20a93fee9abcfd8b2de8a2a23027
Carlos Zhao <Carlos@Hongzhis-MacBook-Pro.local>
2021-03-02 12:05:17 -0500

    add user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,25 @@
 class User < ApplicationRecord
+
+  # == Extensions ===========================================================
+
+  has_secure_password
+
+  # == Validations ==========================================================
+
+  validates :username, 
+    presence: true,
+    length: {minimum: 2, maximum: 40}, uniqueness: true
+
+  validates :email, 
+    email: true,
+    uniqueness: true,
+    length: { minimum: 2, maximum: 254 },
+    presence: true
+
+  validates :password, 
+    confirmation: true,
+    length: {minimum: 4}
+
+  validates_presence_of :password_confirmation
+  validates_presence_of :password_digest
 end
